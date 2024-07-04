0104d4f9406e04dc0a5987d47f91bdbbc04aeba4
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-07-04 11:09:05 +0100

    Upgrade project to Tailwind CSS and  add shadcn Svelte UI components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="container mx-auto rounded-lg bg-white px-4 py-4 shadow-md dark:bg-gray-800">
+	<main class="mb-8">
+		<h1 class="mb-2 text-3xl font-bold text-blue-600 dark:text-blue-400">
+			Welcome to TSoC's Demo App (built with Sveltekit)
+		</h1>
+		<p class="text-lg text-gray-700 dark:text-gray-300">
+			Embark on a journey to discover the Timechain Summer of Code (TSoC) initiative, dedicated to
+			building a robust BSV developer network.
+		</p>
+	</main>
+
+	<section class="rounded-lg bg-gray-100 p-4 dark:bg-gray-700">
+		<h2 class="mb-2 text-2xl font-semibold text-blue-500 dark:text-blue-300">About TSoC</h2>
+		<p class="text-gray-700 dark:text-gray-300">
+			TSoC is committed to nurturing a dynamic BSV developer community. Through education,
+			mentorship, and open-source collaboration, we uphold Satoshi's Bitcoin vision.
+		</p>
+	</section>
+
+	<section class="mt-8">
+		<h2 class="mb-2 text-2xl font-semibold text-blue-500 dark:text-blue-300">Join Us!</h2>
+		<p class="mb-4 text-gray-700 dark:text-gray-300">
+			Participate in our demo app by contributing names to our growing database. Your involvement
+			helps shape the future of BSV development.
+		</p>
+		<button
+			class="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-200 dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-900"
+			>Add Name</button
+		>
+	</section>
+</div>
